050c421684224d968d4d2708f2103942b5993bd7
Василенко Илья <ivvasilen1@mts.ru>
2025-01-09 22:57:53 +0300

    feat: архивация javadock

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,15 +50,19 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
+tasks.javadoc {
+    destinationDir = file("$buildDir/docs/javadoc")
+}
+
 tasks.register<Zip>("zipJavaDoc") {
     group = "documentation" // Группа, в которой будет отображаться задача
     description = "Packs the generated Javadoc into a zip archive"
 
     dependsOn("javadoc") // Указываем, что задача зависит от выполнения javadoc
 
-    from("build/docs/javadoc") // Исходная папка для упаковки
-    archiveFileName.set("javadoc.zip") // Имя создаваемого архива
-    destinationDirectory.set(layout.buildDirectory.dir("archives")) // Директория, куда будет сохранен архив
+    from(tasks.javadoc.get().destinationDir) // Путь к результатам Javadoc
+    archiveFileName.set("javadoc.zip")
+    destinationDirectory.set(layout.buildDirectory.dir("archives"))
 }
 
 tasks.spotbugsMain {
